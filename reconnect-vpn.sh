819a85afaa06bb38ef42aad6efc3a5952e051376
Alif Faishol <alifaishol@gmail.com>
2020-06-21 07:38:36 +0700

    Define config manually

--- reconnect-vpn.sh
+++ reconnect-vpn.sh
@@ -11,78 +11,25 @@
 #===============================================================================
 
 #-------------------------------------------------------------------------------
-#  Pull in VPN config files
+#  Set variables - EDIT THIS!
+#  Get the configuration from:
+#  /usr/syno/etc/synovpnclient/l2tp/l2tpclient.conf
+#  /usr/syno/etc/synovpnclient/openvpn/ovpnclient.conf
+#  /usr/syno/etc/synovpnclient/pptp/pptpclient.conf)
+#  PROFILE_PROTOCOL="openvpn"|"l2tp"|"pptp"
 #-------------------------------------------------------------------------------
 
-if [[ -f /usr/syno/etc/synovpnclient/l2tp/l2tpclient.conf ]]; then
-	L2TP_CONFIG=$(cat /usr/syno/etc/synovpnclient/l2tp/l2tpclient.conf)
-else
-	L2TP_CONFIG=""
-fi
-
-if [[ -f /usr/syno/etc/synovpnclient/openvpn/ovpnclient.conf ]]; then
-	OPENVPN_CONFIG=$(cat /usr/syno/etc/synovpnclient/openvpn/ovpnclient.conf)
-else
-	OPENVPN_CONFIG=""
-fi
-
-if [[ -f /usr/syno/etc/synovpnclient/pptp/pptpclient.conf ]]; then
-	PPTP_CONFIG=$(cat /usr/syno/etc/synovpnclient/pptp/pptpclient.conf)
-else
-	PPTP_CONFIG=""
-fi
-
-#-------------------------------------------------------------------------------
-#  Process config files
-#-------------------------------------------------------------------------------
-
-# Concatenate the config files
-CONFIGS_ALL="$L2TP_CONFIG $OPENVPN_CONFIG $PPTP_CONFIG"
-
-# How many VPN profiles are there?
-CONFIGS_QTY=$(echo "$CONFIGS_ALL" | grep -e '\[l' -e '\[o' -e '\[p' | wc -l)
-
-# Only proceed if there is 1 VPN profile
-if [[ $CONFIGS_QTY -eq 1 ]]; then
-	echo "[I] There is 1 VPN profile. Continuing..."
-elif [[ $CONFIGS_QTY -gt 1 ]]; then
-	echo "[E] There are $CONFIGS_QTY VPN profiles. This script supports only 1 VPN profile. Exiting..."
-	exit 1
-else
-	echo "[W] There are 0 VPN profiles. Please create a VPN profile. Exiting..."
-	exit 1
-fi
-
-#-------------------------------------------------------------------------------
-#  Set variables
-#-------------------------------------------------------------------------------
-
-PROFILE_ID=$(echo $CONFIGS_ALL | cut -d "[" -f2 | cut -d "]" -f1)
-PROFILE_NAME=$(echo "$CONFIGS_ALL" | grep -oP "conf_name=+\K\w+")
-PROFILE_RECONNECT=$(echo "$CONFIGS_ALL" | grep -oP "reconnect=+\K\w+")
-
-if [[ $(echo "$CONFIGS_ALL" | grep '\[l') ]]; then
-	PROFILE_PROTOCOL="l2tp"
-elif [[ $(echo "$CONFIGS_ALL" | grep '\[o') ]]; then
-	PROFILE_PROTOCOL="openvpn"
-elif [[ $(echo "$CONFIGS_ALL" | grep '\[p') ]]; then
-	PROFILE_PROTOCOL="pptp"
-fi
+PROFILE_ID=
+PROFILE_NAME=
+PROFILE_PROTOCOL=
 
 #-------------------------------------------------------------------------------
 #  Check the VPN connection
 #-------------------------------------------------------------------------------
 
-if ping -c1 192.168.0.157 >/dev/null; then
+if [[ $(/usr/syno/bin/synovpnc get_conn | grep Uptime) ]] && ping -c1 8.8.8.8 >/dev/null; then
 	echo "[I] VPN is already connected. Exiting..."
 	exit 0
-fi
-
-if [[ $PROFILE_RECONNECT != "yes" ]]; then
-	echo "[W] VPN is not connected, but reconnect is disabled. Please enable reconnect for for the \"$PROFILE_NAME\" VPN profile. Exiting..."
-	exit 1
-else
-	echo "[W] VPN is not connected. Attempting to reconnect..."
 fi
 
 #-------------------------------------------------------------------------------
